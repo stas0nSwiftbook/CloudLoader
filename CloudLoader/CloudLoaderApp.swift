84a549f44f1b57ccdad332214bdc0c71d0057686
Stanislav Buinovsky <sbuinovsky@gmail.com>
2022-02-07 12:34:50 +0300

    Initial Commit

---
+++ CloudLoader/CloudLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudLoaderApp.swift
+//  CloudLoader
+//
+//  Created by Станислав Буйновский on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CloudLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
